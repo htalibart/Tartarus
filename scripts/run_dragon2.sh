b8a377e5233c2e649de49da525b0bfff61c6c087
Hugo Talibart (hugo.talibart@ulb.be) <htalibar@dragon2.umons.ac.be>
2025-04-11 15:41:26 +0200

    fix run dragon2

--- scripts/run_dragon2.sh
+++ scripts/run_dragon2.sh
@@ -5,12 +5,20 @@
 #SBATCH --ntasks=1
 #SBATCH --mem-per-cpu=5120 # 5GB
 #SBATCH --partition=gpu
-#SBATCH --gres="gpu:1"
+#SBATCH --gres=gpu:v100:0
 #
 #SBATCH --mail-user=hugo.talibart@ulb.be
 #SBATCH --mail-type=ALL
 #
 #SBATCH --output=logs/output_fitness_%x.out
 
-singularity exec tartarus_singularity.sif python /benchmark/scripts/fitness_test.py
-singularity exec tartarus_singularity.sif python /benchmark/scripts/cuda_test.py
+
+module load CUDA/12.1.1
+module load cuDNN/8.9.2.26-CUDA-12.1.1
+
+main_dir=/CECI/home/users/h/t/htalibar/Tartarus
+
+singularity exec ${main_dir}/tartarus_singularity.sif python /benchmark/scripts/fitness_test.py
+singularity exec ${main_dir}/tartarus_singularity.sif python /benchmark/scripts/cuda_test.py
+
+#python /CECI/home/users/h/t/htalibar/Tartarus/scripts/cuda_test.py 
