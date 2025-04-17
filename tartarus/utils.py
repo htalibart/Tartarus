291ac725813457423f61341e6ddc95d9cfc2a44f
Hugo Talibart <hugo.talibart@gmail.com>
2025-04-17 07:43:16 +0000

    fix: retab

--- tartarus/utils.py
+++ tartarus/utils.py
@@ -6,17 +6,17 @@ from subprocess import DEVNULL
 
 @contextmanager
 def suppress_output(verbose):
-    """Suppress output when """
-    if verbose:
-        pass
-    else:
-        with open(devnull, 'w') as fnull:
-            with redirect_stderr(fnull) as err, redirect_stdout(fnull) as out:
-                yield (err, out)
+	"""Suppress output when """
+	if verbose:
+		pass
+	else:
+		with open(devnull, 'w') as fnull:
+			with redirect_stderr(fnull) as err, redirect_stdout(fnull) as out:
+				yield (err, out)
 
 def run_command(command, verbose):
-    if verbose:
-        subprocess.run(command, shell=True)
-    else:
-        subprocess.run(command, shell=True, stdout=DEVNULL, stderr=DEVNULL)
+	if verbose:
+		subprocess.run(command, shell=True)
+	else:
+		subprocess.run(command, shell=True, stdout=DEVNULL, stderr=DEVNULL)
 
