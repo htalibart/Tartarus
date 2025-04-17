5c1c959cd7f2624dd26b64d54c58a8642ddacc69
Hugo Talibart <hugo.talibart@gmail.com>
2025-04-17 08:00:21 +0000

    fix: raise RuntimeError when command failed

--- tartarus/utils.py
+++ tartarus/utils.py
@@ -2,7 +2,7 @@ from contextlib import contextmanager, redirect_stderr, redirect_stdout
 from os import devnull
 
 import subprocess
-from subprocess import DEVNULL
+from subprocess import DEVNULL, Popen, PIPE
 
 @contextmanager
 def suppress_output(verbose):
@@ -14,9 +14,19 @@ def suppress_output(verbose):
 			with redirect_stderr(fnull) as err, redirect_stdout(fnull) as out:
 				yield (err, out)
 
-def run_command(command, verbose):
-	if verbose:
-		subprocess.run(command, shell=True)
-	else:
-		subprocess.run(command, shell=True, stdout=DEVNULL, stderr=DEVNULL)
+#def run_command(command, verbose):
+#	if verbose:
+#		subprocess.run(command, shell=True)
+#	else:
+#		subprocess.run(command, shell=True, stdout=DEVNULL, stderr=DEVNULL)
+#
 
+
+def run_command(command, verbose):
+	p = Popen(command, stdout=PIPE, stderr=PIPE, shell=True)
+	output, error = p.communicate()
+	if verbose:
+		print(output.decode())
+	if p.returncode != 0:
+		print(error.decode())
+		raise RuntimeError("error when executing {}".format(command))
